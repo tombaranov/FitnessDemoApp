833d514a681b2f935c2fdd6a5e8212e7faefc7c7
Tom Baranov <g3nz0@yandex.ru>
2026-07-03 23:24:52 +0300

    Добавил Koin для реализации DI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -40,6 +40,7 @@ dependencies {
     implementation(libs.androidx.appcompat)
     implementation(libs.material)
     implementation(libs.androidx.activity)
+    implementation(libs.koin)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
