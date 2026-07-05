3337554412534ca0da308ad467c1a854d4f485f6
Tom Baranov <g3nz0@yandex.ru>
2026-07-05 10:22:30 +0300

    Добавил WorkoutsInteractor и WorkoutsViewModel в DI-модуль

--- app/src/main/java/tombaranov/fitnessdemoapp/workouts/di/DIModule.kt
+++ app/src/main/java/tombaranov/fitnessdemoapp/workouts/di/DIModule.kt
@@ -1,11 +1,20 @@
 package tombaranov.fitnessdemoapp.workouts.di
 
+import org.koin.core.module.dsl.factoryOf
 import org.koin.core.module.dsl.singleOf
+import org.koin.core.module.dsl.viewModelOf
 import org.koin.dsl.bind
 import org.koin.dsl.module
 import tombaranov.fitnessdemoapp.workouts.data.WorkoutsRepositoryImpl
+import tombaranov.fitnessdemoapp.workouts.domain.WorkoutsInteractor
 import tombaranov.fitnessdemoapp.workouts.domain.WorkoutsRepository
+import tombaranov.fitnessdemoapp.workouts.presentation.WorkoutsViewModel
 
 val workoutsModule = module {
+
+    factoryOf(::WorkoutsInteractor)
+
     singleOf(::WorkoutsRepositoryImpl) bind WorkoutsRepository::class
+
+    viewModelOf(::WorkoutsViewModel)
 }
