d56b9303fd65cad83c8703478f54cabdaa5b6dc8
Tom Baranov <g3nz0@yandex.ru>
2026-07-05 10:16:54 +0300

    Добавил UI-модель для отображения тренировок

--- app/src/main/java/tombaranov/fitnessdemoapp/workouts/domain/Workout.kt
+++ app/src/main/java/tombaranov/fitnessdemoapp/workouts/domain/Workout.kt
@@ -1,5 +1,8 @@
 package tombaranov.fitnessdemoapp.workouts.domain
 
+import tombaranov.fitnessdemoapp.R
+import tombaranov.fitnessdemoapp.workouts.presentation.WorkoutUiModel
+
 data class Workout(
     val id: Int,
     val title: String,
@@ -10,4 +13,21 @@ data class Workout(
 
 enum class Type {
     Workout, Live, Complex
+}
+
+fun Workout.toUiModel(): WorkoutUiModel {
+
+    val type = when (this.type) {
+        Type.Workout -> R.string.workouts_screen_workut_type_training
+        Type.Live -> R.string.workouts_screen_workut_type_live
+        Type.Complex -> R.string.workouts_screen_workut_type_complex
+    }
+
+    return WorkoutUiModel(
+        id = this.id,
+        title = this.title,
+        description = this.description,
+        type = type,
+        duration = this.duration,
+    )
 }
---
+++ app/src/main/java/tombaranov/fitnessdemoapp/workouts/presentation/WorkoutUiModel.kt
@@ -0,0 +1,12 @@
+package tombaranov.fitnessdemoapp.workouts.presentation
+
+import androidx.annotation.StringRes
+
+data class WorkoutUiModel(
+    val id: Int,
+    val title: String,
+    val description: String?,
+    @StringRes
+    val type: Int,
+    val duration: Int,
+)
