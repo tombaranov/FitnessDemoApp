c97d04e1ce6d069a0a9347ca43131918087d1131
Tom Baranov <g3nz0@yandex.ru>
2026-07-05 14:59:38 +0300

    Добавил зависимости в workoutDetailsModule

--- app/src/main/java/tombaranov/fitnessdemoapp/workoutdetails/di/DIModule.kt
+++ app/src/main/java/tombaranov/fitnessdemoapp/workoutdetails/di/DIModule.kt
@@ -1,10 +1,19 @@
 package tombaranov.fitnessdemoapp.workoutdetails.di
 
+import org.koin.core.module.dsl.factoryOf
+import org.koin.core.module.dsl.singleOf
 import org.koin.core.module.dsl.viewModelOf
+import org.koin.dsl.bind
 import org.koin.dsl.module
+import tombaranov.fitnessdemoapp.workoutdetails.data.WorkoutVideoRepositoryImpl
+import tombaranov.fitnessdemoapp.workoutdetails.domain.WorkoutVideoInteractor
+import tombaranov.fitnessdemoapp.workoutdetails.domain.WorkoutVideoRepository
 import tombaranov.fitnessdemoapp.workoutdetails.presentation.WorkoutDetailsViewModel
 
 val workoutDetailsModule = module {
+    factoryOf(::WorkoutVideoInteractor)
+
+    singleOf(::WorkoutVideoRepositoryImpl) bind WorkoutVideoRepository::class
 
     viewModelOf(::WorkoutDetailsViewModel)
 }
