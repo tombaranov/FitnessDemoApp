267019864f01ead2514a2b31999bb0866364dca2
Tom Baranov <g3nz0@yandex.ru>
2026-07-05 10:18:25 +0300

    Добавил DispatchersModule в MainApplication

--- app/src/main/java/tombaranov/fitnessdemoapp/root/MainApplication.kt
+++ app/src/main/java/tombaranov/fitnessdemoapp/root/MainApplication.kt
@@ -3,6 +3,7 @@ package tombaranov.fitnessdemoapp.root
 import android.app.Application
 import org.koin.android.ext.koin.androidContext
 import org.koin.core.context.startKoin
+import tombaranov.fitnessdemoapp.core.coroutines.dispatchersModule
 import tombaranov.fitnessdemoapp.workouts.di.workoutsModule
 
 class MainApplication : Application() {
@@ -11,7 +12,7 @@ class MainApplication : Application() {
         super.onCreate()
         startKoin {
             androidContext(this@MainApplication)
-            modules(workoutsModule)
+            modules(dispatchersModule, workoutsModule)
         }
     }
 }
