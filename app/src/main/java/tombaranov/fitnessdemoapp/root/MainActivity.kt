ad055dcc3a2e0d21fca0f439e2f03c237807c0a0
Tom Baranov <g3nz0@yandex.ru>
2026-07-05 12:57:22 +0300

    Исправил отображение контента под системными барами

--- app/src/main/java/tombaranov/fitnessdemoapp/root/MainActivity.kt
+++ app/src/main/java/tombaranov/fitnessdemoapp/root/MainActivity.kt
@@ -1,8 +1,12 @@
 package tombaranov.fitnessdemoapp.root
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.updatePadding
 import tombaranov.fitnessdemoapp.R
 
 class MainActivity : AppCompatActivity() {
@@ -10,5 +14,21 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(R.layout.activity_main)
+        setUpSystemBarsInsets()
+    }
+
+    private fun setUpSystemBarsInsets() {
+        val root = findViewById<View>(R.id.nav_host_container)
+
+        ViewCompat.setOnApplyWindowInsetsListener(root) { view, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+
+            view.updatePadding(
+                top = systemBars.top,
+                bottom = systemBars.bottom
+            )
+
+            insets
+        }
     }
 }
