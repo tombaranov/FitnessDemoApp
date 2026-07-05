3bbf425163a87fa68d4c58ea695712a493118459
Tom Baranov <g3nz0@yandex.ru>
2026-07-05 10:21:23 +0300

    Реализовал скелет WorkoutsViewModel

--- app/src/main/java/tombaranov/fitnessdemoapp/workouts/presentation/WorkoutsViewModel.kt
+++ app/src/main/java/tombaranov/fitnessdemoapp/workouts/presentation/WorkoutsViewModel.kt
@@ -1,5 +1,26 @@
 package tombaranov.fitnessdemoapp.workouts.presentation
 
 import androidx.lifecycle.ViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
 
-class WorkoutsViewModel : ViewModel() {}
+class WorkoutsViewModel : ViewModel() {
+
+
+    private val _uiState: MutableStateFlow<WorkoutsUiState> =
+        MutableStateFlow(WorkoutsUiState.Loading)
+    val uiState: StateFlow<WorkoutsUiState>
+        get() = _uiState.asStateFlow()
+}
+
+sealed interface WorkoutsUiState {
+
+    object Loading : WorkoutsUiState
+
+    data class Loaded(
+        val workouts: List<WorkoutUiModel>
+    ) : WorkoutsUiState
+
+    object LoadingFailed : WorkoutsUiState
+}
