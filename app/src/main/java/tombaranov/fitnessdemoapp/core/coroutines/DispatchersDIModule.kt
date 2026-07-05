0a2f549d82cd40b5b5a02935352d6851782ed9e7
Tom Baranov <g3nz0@yandex.ru>
2026-07-05 10:16:16 +0300

    Добавил модуль для Dispatchers

---
+++ app/src/main/java/tombaranov/fitnessdemoapp/core/coroutines/DispatchersDIModule.kt
@@ -0,0 +1,20 @@
+package tombaranov.fitnessdemoapp.core.coroutines
+
+import kotlinx.coroutines.Dispatchers
+import org.koin.core.qualifier.named
+import org.koin.dsl.module
+
+object DispatchersQualifiers {
+    const val IO = "IO"
+    const val MAIN = "MAIN"
+    const val DEFAULT = "DEFAULT"
+}
+
+
+val dispatchersModule = module {
+    single(named(DispatchersQualifiers.IO)) { Dispatchers.IO }
+
+    single(named(DispatchersQualifiers.DEFAULT)) { Dispatchers.Default }
+
+    single(named(DispatchersQualifiers.MAIN)) { Dispatchers.Main }
+}
