b33c179760788f2b8c148a9735b5a8a080291e7e
Tom Baranov <g3nz0@yandex.ru>
2026-07-10 14:38:00 +0300

    Вынес плеер в VideoPlayerManager

---
+++ app/src/main/java/tombaranov/fitnessdemoapp/player/VideoPlayerManager.kt
@@ -0,0 +1,56 @@
+package tombaranov.fitnessdemoapp.player
+
+import android.content.Context
+import android.util.Log
+import androidx.annotation.OptIn
+import androidx.core.net.toUri
+import androidx.media3.common.MediaItem
+import androidx.media3.common.PlaybackException
+import androidx.media3.common.Player
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.datasource.DefaultHttpDataSource
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.source.DefaultMediaSourceFactory
+import androidx.media3.ui.PlayerView
+
+class VideoPlayerManager(
+    private val context: Context,
+    private val onError: () -> Unit,
+) {
+
+    private var player: ExoPlayer? = null
+
+    @OptIn(UnstableApi::class)
+    fun attachTo(playerView: PlayerView) {
+        release()
+        val dataSourceFactory = DefaultHttpDataSource.Factory()
+            .setAllowCrossProtocolRedirects(true)
+
+        player = ExoPlayer.Builder(context)
+            .setMediaSourceFactory(DefaultMediaSourceFactory(dataSourceFactory))
+            .build()
+            .also { exoPlayer ->
+                exoPlayer.addListener(object : Player.Listener {
+                    override fun onPlayerError(error: PlaybackException) {
+                        Log.e("Player", "Playback error: ${error.message}", error)
+                        onError()
+                    }
+                })
+            }
+
+        playerView.player = player
+    }
+
+    fun play(videoUrl: String) {
+        val mediaItem = MediaItem.fromUri(videoUrl.toUri())
+        player?.setMediaItem(mediaItem)
+        player?.prepare()
+        player?.playWhenReady = true
+    }
+
+    fun release() {
+        player?.playWhenReady = false
+        player?.release()
+        player = null
+    }
+}
