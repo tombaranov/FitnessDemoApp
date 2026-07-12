b48b94edee56f23a4e1c8c5de09dd1270e494b5a
Tom Baranov <g3nz0@yandex.ru>
2026-07-12 14:22:56 +0300

    Вынес логику создания плеера в фабрику

---
+++ app/src/main/java/tombaranov/fitnessdemoapp/player/ExoPlayerFactory.kt
@@ -0,0 +1,25 @@
+package tombaranov.fitnessdemoapp.player
+
+import android.content.Context
+import androidx.annotation.OptIn
+import androidx.media3.common.Player
+import androidx.media3.common.util.UnstableApi
+import androidx.media3.datasource.DefaultHttpDataSource
+import androidx.media3.exoplayer.ExoPlayer
+import androidx.media3.exoplayer.source.DefaultMediaSourceFactory
+
+class ExoPlayerFactory(
+    private val context: Context,
+) {
+
+    @OptIn(UnstableApi::class)
+    fun create(listener: Player.Listener): ExoPlayer {
+        val dataSourceFactory = DefaultHttpDataSource.Factory()
+            .setAllowCrossProtocolRedirects(true)
+
+        return ExoPlayer.Builder(context)
+            .setMediaSourceFactory(DefaultMediaSourceFactory(dataSourceFactory))
+            .build()
+            .also { it.addListener(listener) }
+    }
+}
